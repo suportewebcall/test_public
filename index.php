baefb4d37fa0c33053e5bc5bbb4584efdc524526
Dagoberto <suporte.webcall@gmail.com>
2022-02-09 22:43:58 -0300

    terceiro commit

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
 
+    teste();
+    echo " Public."
+/**
+ * Descricao
+ *
+ * @author Dagoberto Amorim
+ * @date
+ * @param type  $axxxx descricao
+ * @param type  $axxxx descricao
+ * @return type  descricao
+ */
+function teste() {
 
-
-echo "Teste"
+    echo "Teste";
+}
 
 ?>
 
